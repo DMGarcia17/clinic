bddd3aa04b2f841828ac648d5e392af93bad61ee
DMGarcia17 <37032838+DMGarcia17@users.noreply.github.com>
2022-11-11 23:22:04 -0600

    Fixed icons for main menu

--- fragments/sidebar.php
+++ fragments/sidebar.php
@@ -37,25 +37,25 @@
             <ul class="nav nav-treeview">
                 <li class="nav-item">
                 <a href="http://localhost/clinic/pages/clinics.php" class="nav-link" id="clinicsTagMenu">
-                    <em class="fal fa-hospitals nav-icon"></em>
+                    <em class="fa fa-hospital nav-icon"></em>
                     <p>Cl&iacute;nicas</p>
                 </a>
                 </li>
                 <li class="nav-item">
                     <a href="http://localhost/clinic/pages/diseases.php" class="nav-link" id="diseasesTagMenu">
-                        <em class="fal fa-disease nav-icon"></em>
+                        <em class="fa fa-disease nav-icon"></em>
                         <p>Enfermedades</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="http://localhost/clinic/pages/allergies.php" class="nav-link" id="allergiesTagMenu">
-                        <em class="fal fa-disease nav-icon"></em>
+                        <em class="fa fa-hand-dots nav-icon"></em>
                         <p>Alergias</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="http://localhost/clinic/pages/treatments.php" class="nav-link" id="treatmentsTagMenu">
-                        <em class="fal fa-disease nav-icon"></em>
+                        <em class="fa fa-syringe nav-icon"></em>
                         <p>Tratamientos</p>
                     </a>
                 </li>
